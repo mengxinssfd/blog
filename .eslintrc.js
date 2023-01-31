3cf8ce8c59523cbe0f91162cb2e0e23bb0c5beff
dyh_a <xinzon32177@163.com>
2023-01-31 14:36:45 +0800

    chore: update front eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
   /* 优先级低于parse的语法解析配置 */
   parserOptions: {
-    parser: "@typescript-eslint/parser",
+    parser: '@typescript-eslint/parser',
   },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended", // typescript-eslint推荐规则
-    "prettier",
-    "plugin:prettier/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended', // typescript-eslint推荐规则
+    'prettier',
+    'plugin:prettier/recommended',
   ],
   env: {
     browser: true,
@@ -17,30 +17,63 @@ module.exports = {
   },
   rules: {
     // 禁止使用 var
-    "no-var": "error",
-    // 不使用结尾分号
-    semi: "off",
-    "no-case-declarations": "off",
+    'no-var': 'error',
+    // 使用结尾分号
+    semi: 'off',
+    'no-case-declarations': 'off',
     // 优先使用 interface 而不是 type
-    "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/ban-types": "off",
-    "@typescript-eslint/no-unused-vars": "error",
-    "@typescript-eslint/no-this-alias": "off",
+    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-this-alias': 'off',
   },
   overrides: [
     {
-      files: ["**/__tests__/**"],
+      files: ['apps/front/**'],
+      /* 指定如何解析语法。 */
+      parser: 'vue-eslint-parser',
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+        'plugin:prettier/recommended',
+        // vue nuxt
+        '@nuxtjs/eslint-config-typescript',
+        'plugin:nuxt/recommended',
+        'plugin:vue/vue3-recommended',
+      ],
       rules: {
-        "@typescript-eslint/no-empty-function": "off",
-        "@typescript-eslint/no-non-null-assertion": "off",
+        // 使用结尾分号
+        semi: 'off',
+        'vue/no-v-model-argument': 'off',
+        'vue/html-indent': [
+          'error',
+          2,
+          {
+            attribute: 1,
+            baseIndent: 1,
+            closeBracket: 0,
+            alignAttributesVertically: true,
+            ignores: [],
+          },
+        ],
+        'vue/max-attributes-per-line': [
+          'error',
+          {
+            singleline: 10,
+            multiline: 1,
+          },
+        ],
+        'vue/multi-word-component-names': 'off',
       },
     },
     {
-      files: ["**/scripts/**.[jt]s", "rollup.config.js"],
+      files: ['**/__tests__/**'],
       rules: {
-        "@typescript-eslint/no-var-requires": "off",
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
       },
     },
   ],
