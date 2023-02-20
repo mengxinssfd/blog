aa45a81dd4777c67c35bbfa037ebcbaf59a1b73d
dyh_a <xinzon32177@163.com>
2023-02-21 02:53:28 +0800

    refactor(api): 更新权限校验规则

--- apps/api/src/guards/policies/casl-ability.factory.ts
+++ apps/api/src/guards/policies/casl-ability.factory.ts
@@ -39,9 +39,14 @@ export class CaslAbilityFactory {
     cannot(Action.Update, UserEntity, 'muted', { role: ROLE.superAdmin }).because(
       '不能禁言superAdmin',
     );
-    if (user.role !== ROLE.superAdmin) {
+
+    if (![ROLE.superAdmin, ROLE.admin].includes(user.role)) {
       can(Action.Update, UserEntity, { id: user.id }).because(
         '只有该用户或管理员才可以更新自己的信息',
+      );
+      cannot(Action.Update, UserEntity, { id: { $ne: user.id } }).because('禁止修改其他账号信息');
+      cannot(Action.Update, UserEntity, 'password', { id: { $ne: user.id } }).because(
+        '禁止修改其它账号的密码',
       );
       cannot(Action.Update, UserEntity, 'muted').because('管理员才可以修改用户是否禁言');
 
