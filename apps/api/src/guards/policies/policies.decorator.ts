07a74cc7b2fb27e035e68ec33774675d6a4a3526
dyh_a <xinzon32177@163.com>
2023-02-20 17:17:00 +0800

    feat(api): 新增与`PoliciesGuard`配套的装饰器`CheckPoliciesError`

--- apps/api/src/guards/policies/policies.decorator.ts
+++ apps/api/src/guards/policies/policies.decorator.ts
@@ -1,10 +1,11 @@
 import { SetMetadata } from '@nestjs/common';
 import { AppAbility } from '@/guards/policies/casl-ability.factory';
+import { ForbiddenError } from '@casl/ability';
 
 /**
  * 函数式写法
  */
-export type PolicyHandlerCallback = (ability: AppAbility) => boolean | void;
+export type PolicyHandlerCallback = (ability: AppAbility) => boolean;
 
 /**
  * 类写法
@@ -25,3 +26,17 @@ export const CHECK_POLICIES_KEY = 'check_policies';
  */
 export const CheckPolicies = (...handlers: PolicyHandler[]) =>
   SetMetadata(CHECK_POLICIES_KEY, handlers);
+
+export type PolicyThrowHandlerCallback = (error: ForbiddenError<AppAbility>) => void;
+
+/**
+ * PoliciesGuard配套装饰器
+ * ---
+ * 可以使用 throwUnlessCan，抛出 rule 的 reason
+ */
+export const CheckPoliciesError = (...handlers: PolicyThrowHandlerCallback[]) => {
+  function handlerWrapper(handler: PolicyThrowHandlerCallback) {
+    return (ability: AppAbility) => handler(ForbiddenError.from(ability));
+  }
+  return SetMetadata(CHECK_POLICIES_KEY, handlers.map(handlerWrapper));
+};
