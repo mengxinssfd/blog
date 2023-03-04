5066f9b4fc5829c354d42786abba7a7d1392e32d
dyh_a <xinzon32177@163.com>
2023-03-04 21:40:14 +0800

    fix(front): 修复前端包类型错误

--- apps/front/app.vue
+++ apps/front/app.vue
@@ -4,7 +4,7 @@
   </div>
 </template>
 <script lang="ts" setup>
-import type { UserEntity } from 'blog-entities';
+import type { UserEntity } from '@blog/entities';
 
 const user: Partial<UserEntity> = { id: 1 };
 console.log('1111', user);
