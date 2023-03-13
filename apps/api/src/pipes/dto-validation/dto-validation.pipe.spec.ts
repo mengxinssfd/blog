314a38e49a0c4b3ba082bc49163573f12c2d34bc
dyh_a <xinzon32177@163.com>
2023-03-13 20:15:43 +0800

    chore(api): 清理多余的测试文件

--- apps/api/src/pipes/dto-validation/dto-validation.pipe.spec.ts
+++
@@ -1,7 +0,0 @@
-import { DtoValidationPipe } from './dto-validation.pipe';
-
-describe('ValidationPipe', () => {
-  it('should be defined', () => {
-    expect(new DtoValidationPipe()).toBeDefined();
-  });
-});
