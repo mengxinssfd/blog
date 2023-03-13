b5f3e691228c48a8e277289aada4baf17e7b2573
dyh_a <xinzon32177@163.com>
2023-03-13 20:15:54 +0800

    chore(api): 清理多余的log

--- apps/api/src/guards/throttler-behind-proxy.guard.ts
+++ apps/api/src/guards/throttler-behind-proxy.guard.ts
@@ -10,7 +10,7 @@ export class ThrottlerBehindProxyGuard extends ThrottlerGuard {
     req: IncomingMessage & { ips: string[]; ip: string; user?: UserEntity },
   ): string {
     const ip = getIp(req);
-    console.log('ThrottlerBehindProxyGuard', req.ips, ip, req.user);
+    // console.log('ThrottlerBehindProxyGuard', req.ips, ip, req.user);
     return req.user?.id.toString() ?? (req.ips.join('|') || ip); // individualize IP extraction to meet your own needs
   }
 }
