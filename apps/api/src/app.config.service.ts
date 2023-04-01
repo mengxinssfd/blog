9bec8fdb216fc2ceaa57d98dfde48965084b8958
dyh_a <xinzon32177@163.com>
2023-04-02 00:23:11 +0800

    refactor(api): 为`AppConfigService`添加体操

--- apps/api/src/app.config.service.ts
+++ apps/api/src/app.config.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { Configuration } from './config/configuration';
+import type { Configuration } from './config/configuration';
+import type { Path } from '@nestjs/config';
+import { PathValue } from '@nestjs/config/dist/types';
 
 // import { EnvironmentVariables } from './env/env.variables';
 // type EnvConfig = EnvironmentVariables & Configuration;
 
 @Injectable()
 export class AppConfigService<T extends Configuration = Configuration> extends ConfigService<T> {
-  val<K extends keyof T>(property: K, defaultValue?: T[K]): T[K] {
+  val<P extends Path<T>, R = PathValue<T, P>>(property: P, defaultValue?: R): R {
     return super.get(property as any, defaultValue) as any;
   }
 }
