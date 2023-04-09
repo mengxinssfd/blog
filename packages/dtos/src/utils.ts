869c86dff7cb1e5f58b18c2a7b9bda5f1dadded4
dyh_a <xinzon32177@163.com>
2023-04-10 01:44:54 +0800

    feat(pkg/dtos): custom IsOptional

--- packages/dtos/src/utils.ts
+++ packages/dtos/src/utils.ts
@@ -1,11 +1,21 @@
 import type { Transform } from 'class-transformer';
+import { ValidateIf, ValidationOptions } from 'class-validator';
 
 export const strToNum: Parameters<typeof Transform>[0] = (params) => {
   if (params.value) params.value = Number(params.value);
   return params.value;
 };
 export const toBool: Parameters<typeof Transform>[0] = (params) => {
-  console.log('bbbbbb', params);
   if (params.value) params.value = Boolean(params.value);
   return params.value;
 };
+
+/**
+ * 跟class-validator官方的认为空字符串有值不一样，自定义的把空字符串当成null、undefined
+ * @see {@link https://github.com/typestack/class-validator/issues/326}
+ */
+export function IsOptional(validationOptions?: ValidationOptions) {
+  return ValidateIf((_, value) => {
+    return value !== null && value !== undefined && value !== '';
+  }, validationOptions);
+}
