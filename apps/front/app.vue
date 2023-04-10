10f9195daadca6b492c89c3a3c02587b8f1edc2d
dyh_a <xinzon32177@163.com>
2023-04-11 02:22:27 +0800

    feat(apps/front): 加载后获取个人信息

--- apps/front/app.vue
+++ apps/front/app.vue
@@ -8,8 +8,16 @@
 <script lang="ts" setup>
 import '~/feature/request/primary/index';
 import type { UserEntity } from '@blog/entities';
+import useUserStore from '~/store/user';
 import 'locss';
 
 const user: Partial<UserEntity> = { id: 1 };
 console.log('1111', user);
+
+onMounted(() => {
+  if (/(win|mac|android)/i.test(navigator.userAgent)) {
+    document.documentElement.classList.add(RegExp.$1.toLowerCase());
+  }
+});
+await useUserStore().getSelfInfo();
 </script>
