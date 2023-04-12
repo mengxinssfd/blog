d377487b4e1dae51d5a5da1aaab886f1a83e38f2
dyh_a <xinzon32177@163.com>
2023-04-13 05:04:57 +0800

    refactor(apps/front): user store

--- apps/front/store/user.ts
+++ apps/front/store/user.ts
@@ -1,36 +1,33 @@
 import { defineStore } from 'pinia';
 import { useAsyncData } from '#app';
 import type { UserEntity } from '@blog/entities';
-import { getSelfInfo, login } from '@blog/apis';
+import { getSelfInfo as getSelfInfoApi, login as loginApi } from '@blog/apis';
 import { Token } from '~/feature/request/primary/token';
 
-const useUserStore = defineStore({
-  id: 'user',
-  state: () => {
-    return {} as UserEntity;
-  },
-  actions: {
-    async login(username: string, password: string) {
-      const res = await login({ username, password });
-      Token.set(res.data.token);
-      this.getSelfInfo();
-    },
-    async getSelfInfo() {
-      console.log('getSelfInfo');
-      try {
-        const {
-          data: {
-            value: {
-              data: { user },
-            },
-          },
-        } = await useAsyncData(() => getSelfInfo(), { default: () => ({ data: {} } as any) });
-        this.$state = user;
-      } catch (e) {
-        console.log(e);
-      }
-    },
-  },
+const useUserStore = defineStore('user', () => {
+  const $state = ref<UserEntity>({} as UserEntity);
+
+  async function login(username: string, password: string) {
+    const res = await loginApi({ username, password });
+    Token.set(res.data.token);
+    getSelfInfo();
+  }
+  async function getSelfInfo() {
+    try {
+      const { data } = await useAsyncData(() => getSelfInfoApi(), {
+        default: () => ({ data: { user: {} } } as any),
+      });
+      $state.value = data.value.data.user;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  return {
+    $state,
+    login,
+    getSelfInfo,
+  };
 });
 
 export default useUserStore;
