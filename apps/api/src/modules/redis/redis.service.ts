7ada10a420a8f5c5cc49fb56aec5a61fff588f1f
dyh_a <xinzon32177@163.com>
2023-04-15 05:19:14 +0800

    feat(apps/api): 使用redis缓存每日Bing图

--- apps/api/src/modules/redis/redis.service.ts
+++ apps/api/src/modules/redis/redis.service.ts
@@ -3,6 +3,7 @@ import { InjectRedis } from '@liaoliaots/nestjs-redis';
 import Redis from 'ioredis';
 import { UserEntity } from '@blog/entities';
 import { getTimePeriodConst } from '@tool-pack/basic';
+import { Logger } from '@/utils/log4js';
 
 type BaseUser = Pick<UserEntity, 'id'>;
 
@@ -11,7 +12,7 @@ export class AppRedisService {
   constructor(@InjectRedis() private readonly redis: Redis) {}
 
   private getTokenKeyName(user: BaseUser): string {
-    return 'UserToken_' + user.id;
+    return 'tokens:' + user.id;
   }
 
   getToken(user: BaseUser): Promise<string | null> {
@@ -24,5 +25,16 @@ export class AppRedisService {
 
   delToken(user: BaseUser): Promise<number> {
     return this.redis.del(this.getTokenKeyName(user));
+  }
+
+  setDailyImg(imgList: string) {
+    const now = new Date();
+    const tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
+    const seconds = ~~((tomorrow.getTime() - now.getTime()) / 1000);
+    Logger.info('保存每日Bing图', seconds, imgList);
+    return this.redis.set('dailyImg', imgList, 'EX', seconds);
+  }
+  getDailyImg() {
+    return this.redis.get('dailyImg');
   }
 }
--- apps/api/src/routers/daily-img/daily-img.service.ts
+++ apps/api/src/routers/daily-img/daily-img.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { httpsGet } from '@/utils/utils';
+import { AppRedisService } from '@/modules/redis/redis.service';
 
 @Injectable()
 export class DailyImgService {
-  findOne() {
-    return httpsGet('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=7');
+  constructor(private readonly redisService: AppRedisService) {}
+  async findOne() {
+    const redisDailyImg = await this.redisService.getDailyImg();
+    if (redisDailyImg) return JSON.parse(redisDailyImg);
+
+    const dailyImg = await httpsGet('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=7');
+    this.redisService.setDailyImg(JSON.stringify(dailyImg));
+
+    return dailyImg;
   }
 }
