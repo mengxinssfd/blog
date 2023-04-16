995530bf2e1ca2fb6d41b8f950765566c81f8916
dyh_a <xinzon32177@163.com>
2023-04-16 14:14:52 +0800

    feat(apps/front): 站点配置

--- apps/front/env.d.ts
+++ apps/front/env.d.ts
@@ -1,32 +1,40 @@
 // 参考文档：https://cn.vitejs.dev/guide/env-and-mode.html
 
+export interface Env {
+  readonly VITE_MODE_NAME: 'production' | 'development';
+  // axios的base url
+  readonly VITE_BASE_URL: string;
+  // html lang
+  readonly VITE_APP_LANG: string;
+  // vite config的base
+  readonly VITE_BASE_PATH: string;
+  // 移除console.log
+  readonly VITE_BUILD_DROP_CONSOLE: 'true' | void;
+  // 移除debugger
+  readonly VITE_BUILD_DROP_DEBUGGER: 'true' | void;
+  // sourcemap；开发环境开启less sourcemap，生产环境开启js sourcemap
+  readonly VITE_SOURCEMAP: 'true' | void;
+  // 是否使用rem
+  readonly VITE_USE_REM: 'true' | void;
+  // 是否使用proxy
+  readonly VITE_USE_PROXY: 'true' | void;
+  readonly VITE_PROXY_TARGET: string;
+  // 备案号
+  readonly VITE_CASE_NUMBER: string;
+
+  // cdn
+  readonly VITE_SITE_CDN?: string;
+  // # 网站名
+  VITE_SITE_NAME: string;
+  // # 网站描述
+  VITE_SITE_DESC: string;
+  // # 网站关键词
+  VITE_SITE_KEYWORD: string;
+}
+
 // import.meta.env.VITE_APP_TITLE 使用时的ts类型提示
 declare global {
-  interface ImportMetaEnv {
-    readonly VITE_MODE_NAME: 'production' | 'development';
-    // html title
-    readonly VITE_APP_TITLE: string;
-    // axios的base url
-    readonly VITE_BASE_URL: string;
-    // html lang
-    readonly VITE_APP_LANG: string;
-    // vite config的base
-    readonly VITE_BASE_PATH: string;
-    // 移除console.log
-    readonly VITE_BUILD_DROP_CONSOLE: 'true' | void;
-    // 移除debugger
-    readonly VITE_BUILD_DROP_DEBUGGER: 'true' | void;
-    // sourcemap；开发环境开启less sourcemap，生产环境开启js sourcemap
-    readonly VITE_SOURCEMAP: 'true' | void;
-    // 是否使用rem
-    readonly VITE_USE_REM: 'true' | void;
-    // 是否使用proxy
-    readonly VITE_USE_PROXY: 'true' | void;
-    readonly VITE_PROXY_TARGET: string;
-    // 备案号
-    readonly VITE_CASE_NUMBER: string;
-  }
-
+  interface ImportMetaEnv extends Env {}
   interface ImportMeta {
     readonly env: ImportMetaEnv;
   }
--- apps/front/nuxt.config.ts
+++ apps/front/nuxt.config.ts
@@ -1,8 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import * as Path from 'path';
 import * as Fs from 'fs';
+import { Env } from '~/env';
 
 const pkgs = Fs.readdirSync(Path.resolve(__dirname, '../../packages'));
+
+const env = (process as any).env as Env;
+
+const lifecycle = process.env.npm_lifecycle_event;
+const isBuild = Boolean(lifecycle && ['build', 'generate'].includes(lifecycle));
 
 export default defineNuxtConfig({
   css: ['~/styles/index.scss'],
@@ -18,5 +24,39 @@ export default defineNuxtConfig({
     // Multiple conflicting contents for sourcemap source
     // https://github.com/nuxt/nuxt/issues/14981
     treeshakeClientOnly: false,
+  },
+  app: {
+    cdnURL: env.VITE_SITE_CDN,
+    head: {
+      meta: [
+        { name: 'baidu-site-verification', content: 'code-xSBd0ubpSQ' },
+        { name: 'description', content: env.VITE_SITE_DESC },
+        { name: 'author', content: 'DYH' },
+        { name: 'keyword', content: env.VITE_SITE_KEYWORD },
+      ],
+      link: [
+        {
+          rel: 'shortcut icon',
+          href: '/favicon.ico',
+        },
+        {
+          rel: 'alternate',
+          href: '/atom.xml',
+          title: env.VITE_SITE_NAME,
+          type: 'application/atom+xml',
+        },
+      ],
+      title: env.VITE_SITE_NAME,
+      titleTemplate: (title) => `${env.VITE_SITE_NAME} - ${title}`,
+    },
+  },
+  // build  from https://github.com/element-plus/element-plus-nuxt-starter
+  build: {
+    // Babel 转译
+    transpile: isBuild ? ['element-plus'] : [],
+  },
+  sourcemap: !isBuild,
+  vite: {
+    esbuild: { drop: isBuild ? ['console', 'debugger'] : [] },
   },
 });
