d3368884707bfb10112a149a82f322f70cb9df76
dyh_a <xinzon32177@163.com>
2023-04-17 10:05:14 +0800

    fix(apps/front): howLongAgo

--- apps/front/feature/utils/utils.ts
+++ apps/front/feature/utils/utils.ts
@@ -1,17 +1,17 @@
-import { howLongAgo as HowLongAgo, formatDate } from '@tool-pack/basic';
+import { howLongAgo as HowLongAgo, formatDate, inRange } from '@tool-pack/basic';
 
 export const howLongAgo = (date: string | Date, format?: string) => {
   const _date = typeof date === 'string' ? new Date(date) : date;
   return HowLongAgo(_date, {
-    now: new Date(),
     defaultFormat: format,
     def: '--',
     templates: {
-      second: '刚刚',
       season: '~~',
       year: '~~',
     },
-    filter(res) {
+    filter(res, diff): string {
+      if (inRange(diff, [0, 1000 * 10])) return '刚刚';
+      if (res.endsWith('天前')) return { 1: '昨天', 2: '前天' }[res.replace('天前', '')] || res;
       if (res.endsWith('月前')) return formatDate(_date);
       return res;
     },
