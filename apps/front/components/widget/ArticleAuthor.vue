d9f61cc59464ca410e83e92aad99dddb0b1563ea
dyh_a <xinzon32177@163.com>
2023-04-20 00:44:49 +0800

    feat(apps/front): 文章作者侧边组件

---
+++ apps/front/components/widget/ArticleAuthor.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import * as Vue from 'vue';
+import type { ArticleEntity } from '@blog/entities';
+
+const props = defineProps({
+  article: { type: Object as Vue.PropType<ArticleEntity>, default: () => ({ author: {} }) },
+});
+
+const link = computed(() => '/user/info/' + props.article.author.id);
+</script>
+<template>
+  <Widget>
+    <template #title>
+      <h5 class="widget-title">作者</h5>
+    </template>
+    <div class="widget-content">
+      <nuxt-link :to="link" class="_ flex-col-c" external>
+        <el-avatar :src="article.author.avatar" size="large"></el-avatar>
+        <div class="texts _ flex-1">
+          <div class="title _ ellipsis-2">{{ article.author.nickname }}</div>
+        </div>
+      </nuxt-link>
+    </div>
+  </Widget>
+</template>
+
+<style lang="scss" scoped>
+.widget-content {
+  font-size: 13px;
+  .texts {
+    margin-top: 6px;
+    color: var(--text-color);
+    .title {
+      font-weight: bold;
+    }
+  }
+}
+</style>
