5ec28af018fa4bb19613c4888b3e2a3610df737b
dyh_a <xinzon32177@163.com>
2023-04-20 00:44:24 +0800

    feat(apps/front): 倒计时侧边组件

---
+++ apps/front/components/widget/Countdown.vue
@@ -0,0 +1,118 @@
+<script setup lang="ts">
+import { getTheLastDateOfAMonth, getTimePeriodConst } from '@tool-pack/basic';
+
+function getHoursAgo(date: Date): number {
+  return Number((date.getHours() / 24).toFixed(2));
+}
+
+const now = ref(new Date());
+const searchValue = '{ago}';
+interface Item {
+  template: string;
+  percentage: number;
+  text: string;
+  status: 'success' | 'exception' | 'warning' | '';
+  result(now: Date): { ago: number; percentage: number };
+}
+const list: Array<Item> = [
+  {
+    template: `今天已过去${searchValue}小时`,
+    percentage: 0,
+    status: '',
+    text: '',
+    result(now) {
+      const ago = now.getHours() + Number((now.getMinutes() / 60).toFixed(2));
+      return { ago, percentage: Number(ago) / 24 };
+    },
+  },
+  {
+    template: `本周已过去${searchValue}天`,
+    percentage: 0,
+    status: 'success',
+    text: '',
+    result(now) {
+      const ago = (now.getDay() || 7) - 1 + getHoursAgo(now);
+      return { ago, percentage: Number(ago) / 7 };
+    },
+  },
+  {
+    template: `本月已过去${searchValue}天`,
+    percentage: 0,
+    status: 'exception',
+    text: '',
+    result(now) {
+      const dateCount = getTheLastDateOfAMonth(now).getDate();
+      const ago = now.getDate() - 1 + getHoursAgo(now);
+      return { ago, percentage: Number(ago) / dateCount };
+    },
+  },
+  {
+    template: `今年已过去${searchValue}天`,
+    percentage: 0,
+    status: 'warning',
+    text: '',
+    result(now) {
+      const startOfYear = new Date(now.getFullYear(), 0, 1);
+      const startOfNextYear = new Date(now.getFullYear() + 1, 0, 1);
+      const yearDiff = startOfNextYear.getTime() - startOfYear.getTime();
+      const dateCount = yearDiff / getTimePeriodConst().day;
+      const ago = (now.getTime() - startOfYear.getTime()) / getTimePeriodConst().day;
+      return { ago, percentage: ago / dateCount };
+    },
+  },
+];
+
+const progressList = computed(() => {
+  return list.map<Omit<Item, 'result'>>(({ result, ...rest }) => {
+    const res = result(now.value);
+    const ago = String(Number(res.ago.toFixed(2)));
+    return {
+      ...rest,
+      percentage: Number((res.percentage * 100).toFixed(2)),
+      text: rest.template.replace(searchValue, ago),
+    };
+  });
+});
+
+let timer: ReturnType<typeof setTimeout>;
+const updateDate = () => {
+  now.value = new Date();
+  timer = setTimeout(updateDate, getTimePeriodConst().minute);
+};
+
+onMounted(updateDate);
+onUnmounted(() => clearTimeout(timer));
+</script>
+<template>
+  <Widget>
+    <template #title>
+      <h5 class="widget-title">倒计时</h5>
+    </template>
+    <div class="widget-content">
+      <ul>
+        <li v-for="item in progressList" :key="item.text">
+          <div>{{ item.text }}</div>
+          <el-progress
+            text-inside
+            :stroke-width="20"
+            :percentage="item.percentage"
+            :status="item.status">
+          </el-progress>
+        </li>
+      </ul>
+    </div>
+  </Widget>
+</template>
+
+<style lang="scss" scoped>
+.widget-content {
+  li {
+    margin-top: 10px;
+  }
+  :deep(.el-progress) {
+    + .el-progress {
+      margin-top: 4px;
+    }
+  }
+}
+</style>
