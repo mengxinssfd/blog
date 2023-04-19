29b71a7eac564cfc8e6e08ed223522ef2268a3f7
dyh_a <xinzon32177@163.com>
2023-04-20 00:43:47 +0800

    feat(apps/front): 最新评论侧边组件

---
+++ apps/front/components/widget/RecentComments.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { getRecentComment } from '@blog/apis';
+import { useRequest } from '@request-template/vue3-hooks';
+import { type CommentEntity } from '@blog/entities';
+// table of content
+const router = useRouter();
+const route = useRoute();
+const { data, request } = useRequest(getRecentComment, undefined, []);
+
+interface InnerComment extends CommentEntity {
+  link: string;
+  active: boolean;
+}
+const list = computed(() => {
+  const path = route.path;
+  return data.value.map((item) => {
+    const link = '/article/detail/' + item.articleId;
+    return { ...item, link, active: path === link };
+  }) as InnerComment[];
+});
+
+onMounted(request);
+
+function toArticle(item: InnerComment) {
+  if (item.active) return;
+  router.push(item.link);
+}
+</script>
+<template>
+  <Widget>
+    <template #title>
+      <h5 class="widget-title">最新评论</h5>
+    </template>
+    <div class="widget-content">
+      <ArticleDetailBaseComment
+        v-for="item in list"
+        :key="item.id"
+        :item="item"
+        :class="{ active: item.active }"
+        independent
+        @click-content="toArticle(item)"></ArticleDetailBaseComment>
+    </div>
+  </Widget>
+</template>
+
+<style lang="scss" scoped>
+.c-comment {
+  &:not(.active) {
+    :deep(.content) {
+      cursor: pointer;
+      &:hover {
+        color: var(--theme-color);
+      }
+    }
+  }
+}
+</style>
---
+++ apps/front/components/widget/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="widget board">
+    <div class="title">
+      <slot name="title"></slot>
+    </div>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  sticky: {
+    type: Boolean,
+    default: false,
+  },
+  top: {
+    type: [Number, String],
+    default: '',
+  },
+});
+</script>
+<style lang="scss" scoped>
+.widget {
+  max-width: 300px;
+  margin-top: 0;
+  margin-bottom: 1rem;
+  .title {
+    margin-bottom: 10px;
+    font-size: 1.2rem;
+    font-weight: bold;
+  }
+}
+</style>
