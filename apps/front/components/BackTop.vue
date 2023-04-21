ad67c97160ed33c41d68b27a0866c002a62dd17e
dyh_a <xinzon32177@163.com>
2023-04-22 04:39:22 +0800

    refactor(apps/front): 重构回到顶部组件

--- apps/front/components/BackTop.vue
+++ apps/front/components/BackTop.vue
@@ -1,8 +1,6 @@
 <template>
-  <div class="c-back-top _ pos-trans-c main-width">
-    <div class="_ btn abs-br back-top" :class="{ hide: scrollTop < 600 }" @click="backTop()">
-      <i class="iconfont icon-back-top shake-slow"></i>
-    </div>
+  <div class="_ btn back-top" :class="{ hide: scrollTop < 100 }" @click="backTop()">
+    <i class="iconfont icon-back-top shake-slow"></i>
   </div>
 </template>
 
@@ -11,33 +9,19 @@ import { useWindowScroll } from '~/feature/hooks';
 
 const { scrollTop, backTop } = useWindowScroll();
 </script>
-<style lang="scss">
-.c-back-top {
-  position: fixed !important;
-  z-index: 50;
-  bottom: 60px;
-  .btn.back-top {
-    //margin-right: -30px;
-    background: var(--board-bg-color);
-    color: var(--text-color);
-    border-radius: 6px;
-    box-shadow: 0 0 10px -4px #4280f3;
-    transition: all 0.3s;
-    &:hover {
-      background: #f8f6f6;
-    }
-    i {
-      display: inline-block;
-      padding: 10px;
-    }
-    &.hide {
-      transform: translateY(100%);
-      opacity: 0;
-      pointer-events: none;
-    }
-    @media (max-width: 576px) {
-      margin-right: 20px;
-    }
+<style lang="scss" scoped>
+.btn.back-top {
+  //margin-right: -30px;
+
+  transition: all 0.3s;
+  i {
+    display: inline-block;
+    padding: 10px;
+  }
+  &.hide {
+    transform: translateY(100%);
+    opacity: 0;
+    pointer-events: none;
   }
 }
 </style>
