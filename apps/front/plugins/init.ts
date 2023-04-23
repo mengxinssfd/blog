bb05af2f8ea1051233fc82a33518036b6d99f7c0
dyh_a <xinzon32177@163.com>
2023-04-23 20:00:31 +0800

    refactor(apps/front): elPlugin 重命名为 init

--- apps/front/plugins/init.ts
+++ apps/front/plugins/init.ts
@@ -1,6 +1,8 @@
 import ElementPlus, { ID_INJECTION_KEY } from 'element-plus';
+import { sticky } from '~/feature/directives/sticky';
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp
+    .directive('sticky', sticky)
     .use(ElementPlus)
     // 修复 Looks like you are using server rendering, you must provide a id...
     // 参考 https://github.com/element-plus/element-plus/issues/7963
