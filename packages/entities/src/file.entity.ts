49a7394f58ae83b975fe96c38946cfc4697a1c36
dyh_a <xinzon32177@163.com>
2023-04-23 21:32:59 +0800

    feat(pkgs/entities): file改为表格

--- packages/entities/src/file.entity.ts
+++ packages/entities/src/file.entity.ts
@@ -1,5 +1,19 @@
 import { BlogBaseEntity } from './base.entity';
+import { Column, Entity } from 'typeorm';
 
+@Entity('file')
 export class FileEntity extends BlogBaseEntity {
   static readonly modelName = 'FileEntity' as const;
+
+  @Column('varchar', { comment: 'oss链接', length: 500 })
+  url!: string;
+
+  @Column('int', { comment: '文件大小' })
+  size!: number;
+
+  @Column('varchar', { comment: '文件名', length: 200, unique: true })
+  filename!: string;
+
+  @Column('varchar', { comment: '文件类型', length: 50 })
+  mimetype!: string;
 }
