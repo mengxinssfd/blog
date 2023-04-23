f2003c672b4bcb6fb647848a3bb068bb690a37fd
dyh_a <xinzon32177@163.com>
2023-04-23 20:02:40 +0800

    feat(apps/front): `Aside`

---
+++ apps/front/components/Aside.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+<template>
+  <aside class="c-aside">
+    <slot>
+      <WidgetLoginUser />
+      <WidgetCountdown />
+      <WidgetRecentComments></WidgetRecentComments>
+    </slot>
+  </aside>
+</template>
+
+<style lang="scss">
+.c-aside {
+  margin-top: 1rem;
+  flex-basis: 300px;
+  width: 300px;
+  padding-left: 1rem;
+  transition: transform 0.5s ease-in-out;
+}
+</style>
