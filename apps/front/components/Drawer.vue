0539e82ace2f1b89a810d7362eb9a81bfcd4dd06
dyh_a <xinzon32177@163.com>
2023-04-25 23:50:37 +0800

    feat(apps/front): 抽屉二次封装

---
+++ apps/front/components/Drawer.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-drawer v-model="value" :with-header="false" class="c-drawer" append-to-body v-bind="$attrs">
+    <slot></slot>
+  </el-drawer>
+</template>
+
+<script setup lang="ts">
+/**
+ * 由于el-drawer的modelValue只支持boolean类型，
+ * 在tsx组件中使用时传入ref()会报错，传boolean ref.value会丢失响应，
+ * 而在vue组件中不会，所以要二次封装才能使用
+ *
+ * @see {@link https://element-plus.org/zh-CN/component/drawer.html}
+ */
+
+const props = defineProps({
+  modelValue: {
+    type: [Boolean, Object],
+    default: false,
+  },
+});
+const emit = defineEmits(['update:modelValue']);
+
+const value = computed({
+  get: () => (isRef(props.modelValue) ? props.modelValue.value : props.modelValue) as boolean,
+  set: (v: boolean | Record<string, any>) => emit('update:modelValue', v),
+});
+</script>
+<style lang="scss">
+.c-drawer {
+}
+</style>
