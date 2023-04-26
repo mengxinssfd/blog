5e3813eddb569e8c7ca5590f1abf29f014638a11
dyh_a <xinzon32177@163.com>
2023-04-26 18:16:39 +0800

    feat(apps/front): `Space`组件

---
+++ apps/front/components/Space.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const props = defineProps({
+  width: {
+    type: [Number, String],
+    default: '1rem',
+  },
+});
+const width = computed(() => (typeof props.width === 'number' ? props.width + 'px' : props.width));
+</script>
+<template>
+  <span class="c-space" :style="{ width: width }"> </span>
+</template>
+
+<style lang="scss" scoped>
+.c-space {
+  display: inline-block;
+}
+</style>
