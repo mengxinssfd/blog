47a8381709a84bbfc4227b83fa75f93137d5cf53
dyh_a <xinzon32177@163.com>
2023-04-28 00:36:33 +0800

    feat(pkgs/permission-rules): 调整友链规则

--- packages/permission-rules/src/rules/friend-link.rule.ts
+++ packages/permission-rules/src/rules/friend-link.rule.ts
@@ -3,14 +3,8 @@ import { Action, RuleCreator } from '../types';
 
 const FriendLink = [FriendLinkEntity, FriendLinkEntity.modelName];
 
-export const createFriendLinkRule: RuleCreator = (user, { can, cannot }) => {
-  can([Action.Create, Action.Read, Action.Update], FriendLink);
-
-  if (![ROLE.superAdmin, ROLE.admin].includes(user.role)) {
-    cannot(Action.Update, FriendLinkEntity, { createBy: { $ne: user.id } }).because(
-      '不可更改其他账号的友链',
-    );
-  }
+export const createFriendLinkRule: RuleCreator = (user, { can }) => {
+  can([Action.Create, Action.Read], FriendLink);
 
   if (user.role === ROLE.admin) {
     can([Action.Update, Action.Delete], FriendLink);
