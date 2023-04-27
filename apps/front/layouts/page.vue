ad227bb38d66a1656dbdb42edb21b2e41341a588
dyh_a <xinzon32177@163.com>
2023-04-27 15:38:07 +0800

    feat(apps/front): page layout

---
+++ apps/front/layouts/page.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="page-layout">
+    <slot name="banner"></slot>
+    <div class="main-wrapper _ flex main-width">
+      <main class="_ flex-1">
+        <slot />
+      </main>
+      <Aside><slot name="aside"></slot></Aside>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+<style lang="scss" scoped>
+@import '@/styles/scss.variable.scss';
+
+.page-layout {
+  main {
+    overflow-x: hidden;
+  }
+  :deep(.c-aside) {
+    @media (max-width: $show-aside-on-screen-width) {
+      display: none;
+    }
+  }
+}
+</style>
