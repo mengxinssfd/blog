049a0f62cca8cbd23a2b0dea508fac627fac78aa
dyh_a <xinzon32177@163.com>
2023-04-27 15:41:25 +0800

    refactor(apps/front): 重构首页

--- apps/front/pages/index.vue
+++ apps/front/pages/index.vue
@@ -1,20 +1,22 @@
 <template>
-  <div class="pg index">
-    <Title>Nice's Blog</Title>
-    <IndexBanner :bg-img="TODAY_COVER_URL" :blur="false">
-      <template v-if="false" #bg>
-        <img
-          v-if="false"
-          referrerpolicy="no-referrer"
-          src="http://store.xiaojiju.com/store/bg1.jpg"
-          alt="" />
-      </template>
-      <template #content>
-        <IndexSearchBox v-if="false" />
-      </template>
-    </IndexBanner>
-    <IndexArticleList />
-  </div>
+  <NuxtLayout name="page">
+    <div class="pg index">
+      <Title>Nice's Blog</Title>
+      <IndexBanner v-if="false" :bg-img="TODAY_COVER_URL" :blur="false">
+        <template v-if="false" #bg>
+          <img
+            v-if="false"
+            referrerpolicy="no-referrer"
+            src="http://store.xiaojiju.com/store/bg1.jpg"
+            alt="" />
+        </template>
+        <template #content>
+          <IndexSearchBox v-if="false" />
+        </template>
+      </IndexBanner>
+      <IndexArticleList />
+    </div>
+  </NuxtLayout>
 </template>
 
 <script setup lang="ts">
