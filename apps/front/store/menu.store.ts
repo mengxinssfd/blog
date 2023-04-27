6e36daa0f2131dbf080297a10c94342ec65e7c0a
dyh_a <xinzon32177@163.com>
2023-04-27 15:39:44 +0800

    refactor(apps/front): 重构menu.store

--- apps/front/store/menu.store.ts
+++ apps/front/store/menu.store.ts
@@ -1,21 +1,32 @@
 import { defineStore } from 'pinia';
 import useUserStore from '~/store/user.store';
 import { ROLE } from '@blog/entities';
+import { useToggleState } from '~/feature/hooks';
 
 const useMenuStore = defineStore('menu', () => {
   const route = useRoute();
   const userStore = useUserStore();
-  const menu = computed<Array<{ path: string; title: string; disabled?: boolean }>>(() => {
-    return [
-      { path: '/', title: `首页` },
-      { path: '/friend-link', title: `友链` },
-      { path: '/about', title: `关于` },
-      { path: '/admin', title: '后台', disabled: userStore.user.role !== ROLE.superAdmin },
-    ];
-  });
+  const menu = computed<Array<{ icon: string; path: string; title: string; disabled?: boolean }>>(
+    () => {
+      return [
+        { path: '/', title: `首页`, icon: 'icon-index' },
+        { path: '/friend-link', title: `友链`, icon: 'icon-link' },
+        { path: '/about', title: `关于`, icon: 'icon-user' },
+        {
+          path: '/admin',
+          title: '后台',
+          icon: 'icon-admin',
+          disabled: userStore.user.role !== ROLE.superAdmin,
+        },
+      ];
+    },
+  );
+  const [sideMenuVisible, toggleSideMenuVisible] = useToggleState(false);
 
   return {
     menu,
+    sideMenuVisible,
+    toggleSideMenuVisible,
     isActive(path: string) {
       if (path === '/') return path === route.path;
       return new RegExp(`^${path}`).test(route.path);
