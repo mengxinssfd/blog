4cd8fd764a4cd663b619f0c08003347e0f3af8a4
dyh_a <xinzon32177@163.com>
2023-05-07 02:12:36 +0800

    feat(apps/api): main.ts部分配置从env文件中取

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ENV } from './utils/utils';
 import { iniApp } from '@/init-app';
+import { AppConfigService } from '@/app.config.service';
 
 declare const module: any;
 
@@ -10,13 +11,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   iniApp(app);
+  const configService = app.get(AppConfigService);
 
   // 线上环境不显示文档
   if (ENV.isDev()) {
     // 配置 Swagger
     const config = new DocumentBuilder()
       .addBearerAuth() // 开启 BearerAuth 授权认证
-      .setTitle('Blog')
+      .setTitle(configService.val('app.name'))
       .setDescription('The Blog API description')
       .setVersion('2.0')
       // .addTag('api')
@@ -28,7 +30,7 @@ async function bootstrap() {
 
   // 不加hostname的话，当前地址访问获取不到ip
   // 参考 https://www.cnblogs.com/lpbottle/p/nodejs_get_ip.html
-  await app.listen(3000, '0.0.0.0');
+  await app.listen(configService.val('app.port'), '0.0.0.0');
   // 不加hostname获取的ip => ::ffff:192.xxx.xxx.xxx
   // await app.listen(3000);
 
