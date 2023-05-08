232b1866fe723c8d73c812f0d41c8f95bb2fd652
dyh_a <xinzon32177@163.com>
2023-05-09 04:27:07 +0800

    feat(apps/api): 使用axios代替原来都httpGet

--- apps/api/src/routers/daily-img/daily-img.module.ts
+++ apps/api/src/routers/daily-img/daily-img.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DailyImgService } from './daily-img.service';
 import { DailyImgController } from './daily-img.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [DailyImgController],
   providers: [DailyImgService],
 })
--- apps/api/src/routers/daily-img/daily-img.service.ts
+++ apps/api/src/routers/daily-img/daily-img.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@nestjs/common';
-import { httpsGet } from '@/utils/utils';
 import { AppRedisService } from '@/modules/redis/redis.service';
+import { HttpService } from '@nestjs/axios';
 
 @Injectable()
 export class DailyImgService {
-  constructor(private readonly redisService: AppRedisService) {}
+  constructor(
+    private readonly redisService: AppRedisService,
+    private readonly httpService: HttpService,
+  ) {}
   async findOne() {
     const redisDailyImg = await this.redisService.getDailyImg();
     if (redisDailyImg) return JSON.parse(redisDailyImg);
 
-    const dailyImg = await httpsGet('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=7');
+    const { data: dailyImg } = await this.httpService.axiosRef.get(
+      'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=7',
+    );
+
     this.redisService.setDailyImg(JSON.stringify(dailyImg));
 
     return dailyImg;
