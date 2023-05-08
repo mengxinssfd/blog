841e14eba5838b2d80c06f527f36714c43bdc9fe
dyh_a <xinzon32177@163.com>
2023-05-08 15:05:17 +0800

    feat(apps/front): 评论锚点改阴影为边框

--- apps/front/components/comment/Base.vue
+++ apps/front/components/comment/Base.vue
@@ -145,7 +145,7 @@ defineExpose({ getNickname });
     }
   }
   &.active {
-    box-shadow: 0 0 15px 0 var(--theme-color);
+    border: 1px solid var(--theme-color);
     border-radius: var(--board-radius);
     padding-left: 7px !important;
     margin: 0 -7px;
