0d0dbe998d9cfeb167b0b60d24e650c09227eab9
dyh_a <xinzon32177@163.com>
2023-05-11 02:23:44 +0800

    feat(apps/front): 新增花环头像组件

---
+++ apps/front/components/Avatar.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import * as Vue from 'vue';
+import type { UserEntity } from '@blog/entities';
+
+const props = defineProps({
+  user: { type: Object as Vue.PropType<UserEntity>, default: () => {} },
+  disabled: { type: Boolean, default: false },
+});
+
+const link = computed(() => '/user/info/' + props.user.id);
+</script>
+
+<template>
+  <nuxt-link :disabled="disabled" :to="link" class="c-avatar">
+    <el-avatar :src="user.avatar" size="large"></el-avatar>
+  </nuxt-link>
+</template>
+
+<style lang="scss" scoped>
+.c-avatar {
+  position: relative;
+  display: inline-block;
+  width: 110px;
+  height: 110px;
+  background: url('https://gd-hbimg.huaban.com/dab5816cc966ba8c8d1b78b30853c34c170c6e36143eb-yOrYsm_fw1200webp')
+    no-repeat center;
+  background-size: cover;
+  .el-avatar {
+    position: absolute;
+    top: 29px;
+    left: 29px;
+  }
+
+  &[disabled='true'] {
+    cursor: default;
+  }
+  &:not([disabled='true']):hover {
+    filter: drop-shadow(1px 1px 10px var(--text-color));
+  }
+}
+</style>
