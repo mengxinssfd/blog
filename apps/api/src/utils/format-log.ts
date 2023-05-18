06688e1c8f30a6c6607e0d35de109035f212c9ca
dyh_a <xinzon32177@163.com>
2023-05-18 16:07:07 +0800

    fix(apps/api): 修复logger获取不到ip的问题

--- apps/api/src/utils/format-log.ts
+++ apps/api/src/utils/format-log.ts
@@ -1,13 +1,14 @@
 import type { Request, Response } from 'express';
 import type { IncomingMessage } from 'http';
 import { ServerResponse } from 'http';
+import { getIp } from '@/utils/utils';
 
 export function formatReqLog(req: Request): string {
   // 组装日志信息
   return ` >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     Request original url: ${req.originalUrl}
     Method: ${req.method}
-    IP: ${req.ip}
+    IP: ${getIp(req)}
     uuid: ${req.headers['uuid']}
     refer: ${req.headers.referer}
     Params: ${JSON.stringify(req.params)}
@@ -30,6 +31,7 @@ export function formatResLog(
   return ` <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     Request original url: ${req.originalUrl}
     Method: ${req.method}
+    IP: ${getIp(req)}
     Status code: ${res.statusCode}
     User: ${JSON.stringify((req as any).user)}
     Response: ${JSON.stringify(data)}
