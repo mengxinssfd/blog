d426dfd3aa264f94e763eb2b811c67451dca5595
dyh_a <xinzon32177@163.com>
2023-05-21 14:41:46 +0800

    feat(apps/front): 友链信息添加时可设置email

--- apps/front/components/friend-link/Dialog.vue
+++ apps/front/components/friend-link/Dialog.vue
@@ -1,41 +1,16 @@
-<!--友链新增/编辑弹窗 FriendLinkDialog.vue-->
-<template>
-  <ClientOnly>
-    <el-dialog v-model="visible" class="edit-user-info-dialog" title="添加">
-      <el-form
-        ref="elFormRef"
-        :model="form"
-        :rules="rules"
-        label-width="80px"
-        @submit.stop.prevent
-        @keydown.enter="submit">
-        <el-form-item label="网站链接" prop="link">
-          <el-input v-model="form.link"></el-input>
-          <div>会自动从链接获取网站信息</div>
-        </el-form-item>
-      </el-form>
-      <template #footer>
-        <el-button type="primary" plain @click="hideDialog">取消</el-button>
-        <el-button v-loading="loading" type="primary" @click="submit">确定</el-button>
-      </template>
-    </el-dialog>
-  </ClientOnly>
-</template>
-
 <script lang="ts" setup>
 import { createFriendLink } from '@blog/apis';
 import type { CreateFriendLinkDto } from '@blog/dtos';
+import { isUrl } from '@tool-pack/basic';
+import { Validator } from '~/types';
 
 const props = defineProps({
-  show: {
-    type: Boolean,
-    default: false,
-  },
+  show: { type: Boolean, default: false },
 });
 
 const emits = defineEmits(['update:show', 'success']);
 
-const createFormValue = (): CreateFriendLinkDto => ({ link: '' });
+const createFormValue = (): CreateFriendLinkDto => ({ link: '', email: '' });
 
 const elFormRef = ref();
 const visible = computed({
@@ -43,7 +18,25 @@ const visible = computed({
   get: () => props.show,
 });
 const form = ref<CreateFriendLinkDto>(createFormValue());
-const rules = { link: { required: true, message: '链接不能为空' } };
+const rules = {
+  link: [
+    {
+      required: true,
+      message: '链接不能为空',
+    },
+    {
+      trigger: ['blur'],
+      validator: ((_, value, callback) => {
+        if (!isUrl(value)) {
+          callback(new Error('链接格式错误'));
+        } else {
+          callback();
+        }
+      }) as Validator,
+    },
+  ],
+  email: {},
+};
 const loading = ref(false);
 
 function hideDialog() {
@@ -61,3 +54,29 @@ async function submit() {
   }
 }
 </script>
+<template>
+  <ClientOnly>
+    <el-dialog v-model="visible" class="edit-user-info-dialog" title="添加">
+      <el-form
+        ref="elFormRef"
+        :model="form"
+        :rules="rules"
+        label-width="80px"
+        @submit.stop.prevent
+        @keydown.enter="submit">
+        <el-form-item label="网站链接" prop="link">
+          <el-input v-model="form.link"></el-input>
+          <div>会自动从链接获取网站信息</div>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="form.email"></el-input>
+          <div>接收友链审核信息</div>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <el-button type="primary" plain @click="hideDialog">取消</el-button>
+        <el-button v-loading="loading" type="primary" @click="submit">确定</el-button>
+      </template>
+    </el-dialog>
+  </ClientOnly>
+</template>
