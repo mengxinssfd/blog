3b1925c55a629766bc5ccb060d5de1b16f9a75b8
dyh_a <xinzon32177@163.com>
2023-05-21 14:16:03 +0800

    feat(pkgs/dtos): 申请friend-link添加email选项

--- packages/dtos/src/friend-link/create-friend-link.dto.ts
+++ packages/dtos/src/friend-link/create-friend-link.dto.ts
@@ -1,10 +1,18 @@
-import { IsNotEmpty, MaxLength, IsUrl } from 'class-validator';
+import { IsNotEmpty, MaxLength, IsUrl, IsEmail } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/mapped-types';
+import { FriendLinkEntity } from '@blog/entities';
+import { IsOptional } from '../utils';
 
-export class CreateFriendLinkDto {
+export class CreateFriendLinkDto extends PartialType(FriendLinkEntity) {
   @ApiProperty({ description: '链接', example: '链接' })
   @MaxLength(254, { message: '链接最大长度为254' })
   @IsUrl({}, { message: 'link必须是url' })
   @IsNotEmpty()
-  link!: string;
+  override link!: string;
+
+  @ApiProperty({ description: '邮箱', example: '', required: false })
+  @IsOptional()
+  @IsEmail({}, { message: '邮箱格式不正确' })
+  override email?: string;
 }
