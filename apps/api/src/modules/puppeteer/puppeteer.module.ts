5f38ff0315514618b4601062041b822b89e648ca
dyh_a <xinzon32177@163.com>
2023-05-22 19:53:13 +0800

    refactor(apps/api): 因为nest-puppeteer在测试期间会自动启动，所以直接使用puppeteer代替第三方module

--- apps/api/src/modules/puppeteer/puppeteer.module.ts
+++ apps/api/src/modules/puppeteer/puppeteer.module.ts
@@ -1,40 +1,40 @@
 import { Module } from '@nestjs/common';
-import { PuppeteerModule } from '@mxssfd/nest-puppeteer';
+// import { PuppeteerModule } from '@mxssfd/nest-puppeteer';
 import { AppPuppeteerService } from './puppeteer.service';
 import { FileHelperModule } from '@/modules/file-helper/file-helper.module';
 
 @Module({
   imports: [
     FileHelperModule,
-    PuppeteerModule.forRoot({
-      // pipe: true,
-      headless: 'new',
-      // 在浏览器输入url：`chrome://flags/`可以看到所有args
-      args: [
-        '--disable-notifications',
-        // '--single-process',
-        '--no-first-run',
-        '--no-zygote',
-        // 禁止插件
-        '--disable-extensions',
-        // 禁用沙箱模式，以在Linux服务器上运行Puppeteer时避免一些权限问题。
-        '--no-sandbox',
-        // 禁用setuid沙箱模式，在某些环境下可以提高性能。
-        '--disable-setuid-sandbox',
-        // 禁用/dev/shm使用，以减少内存使用量。
-        '--disable-dev-shm-usage',
-        // 禁用加速的2D画布，以提高截图的质量。
-        '--disable-accelerated-2d-canvas',
-        // 隐藏滚动条，以获得更清晰的截图。
-        '--hide-scrollbars',
-        // 禁用GPU加速，以避免在某些环境下出现兼容性问题
-        '--disable-gpu',
-        // 禁用浏览器通知，以避免在截图时出现干扰。
-        '--disable-notifications',
-        // 禁用浏览器信息栏，以获得更干净的截图。
-        '--disable-infobars',
-      ],
-    }),
+    // PuppeteerModule.forRoot({
+    //   // pipe: true,
+    //   headless: 'new',
+    //   // 在浏览器输入url：`chrome://flags/`可以看到所有args
+    //   args: [
+    //     '--disable-notifications',
+    //     // '--single-process',
+    //     '--no-first-run',
+    //     '--no-zygote',
+    //     // 禁止插件
+    //     '--disable-extensions',
+    //     // 禁用沙箱模式，以在Linux服务器上运行Puppeteer时避免一些权限问题。
+    //     '--no-sandbox',
+    //     // 禁用setuid沙箱模式，在某些环境下可以提高性能。
+    //     '--disable-setuid-sandbox',
+    //     // 禁用/dev/shm使用，以减少内存使用量。
+    //     '--disable-dev-shm-usage',
+    //     // 禁用加速的2D画布，以提高截图的质量。
+    //     '--disable-accelerated-2d-canvas',
+    //     // 隐藏滚动条，以获得更清晰的截图。
+    //     '--hide-scrollbars',
+    //     // 禁用GPU加速，以避免在某些环境下出现兼容性问题
+    //     '--disable-gpu',
+    //     // 禁用浏览器通知，以避免在截图时出现干扰。
+    //     '--disable-notifications',
+    //     // 禁用浏览器信息栏，以获得更干净的截图。
+    //     '--disable-infobars',
+    //   ],
+    // }),
   ],
   providers: [AppPuppeteerService],
   exports: [AppPuppeteerService],
--- apps/api/src/modules/puppeteer/puppeteer.service.ts
+++ apps/api/src/modules/puppeteer/puppeteer.service.ts
@@ -1,16 +1,61 @@
 import { Injectable } from '@nestjs/common';
 import { FriendLinkEntity } from '@blog/entities';
 import { Logger } from '@/utils/log4js';
-import { InjectContext } from '@mxssfd/nest-puppeteer';
-import { BrowserContext } from 'puppeteer';
+// import { InjectContext } from '@mxssfd/nest-puppeteer';
+// import { BrowserContext } from 'puppeteer';
 import { FileHelperService } from '@/modules/file-helper/file-helper.service';
+import puppeteer, { Browser } from 'puppeteer';
 
 @Injectable()
 export class AppPuppeteerService {
+  private browser: Browser | null = null;
+
   constructor(
-    @InjectContext() private readonly browserContext: BrowserContext,
+    // @InjectContext() private readonly browserContext: BrowserContext,
     private readonly fileHelperService: FileHelperService,
   ) {}
+
+  private async getBrowser(): Promise<Browser> {
+    if (this.browser) return this.browser;
+    return (this.browser = await puppeteer.launch({
+      // pipe: true,
+      headless: 'new',
+      // 在浏览器输入url：`chrome://flags/`可以看到所有args
+      args: [
+        '--disable-notifications',
+        // '--single-process',
+        '--no-first-run',
+        '--no-zygote',
+        // 禁止插件
+        '--disable-extensions',
+        // 禁用沙箱模式，以在Linux服务器上运行Puppeteer时避免一些权限问题。
+        '--no-sandbox',
+        // 禁用setuid沙箱模式，在某些环境下可以提高性能。
+        '--disable-setuid-sandbox',
+        // 禁用/dev/shm使用，以减少内存使用量。
+        '--disable-dev-shm-usage',
+        // 禁用加速的2D画布，以提高截图的质量。
+        '--disable-accelerated-2d-canvas',
+        // 隐藏滚动条，以获得更清晰的截图。
+        '--hide-scrollbars',
+        // 禁用GPU加速，以避免在某些环境下出现兼容性问题
+        '--disable-gpu',
+        // 禁用浏览器通知，以避免在截图时出现干扰。
+        '--disable-notifications',
+        // 禁用浏览器信息栏，以获得更干净的截图。
+        '--disable-infobars',
+      ],
+    }));
+  }
+
+  private async newPage() {
+    const browser = await this.getBrowser();
+    return browser.newPage();
+  }
+
+  async destroy() {
+    if (this.browser?.isConnected()) await this.browser.close();
+  }
 
   async getSiteInfoWithScreenshotUrl(link: string, filename?: string) {
     const siteInfo = await this.getSiteInfo(link);
@@ -34,7 +79,7 @@ export class AppPuppeteerService {
 
   async getSiteInfo(link: string) {
     const timeStart = Date.now();
-    const page = await this.browserContext.newPage();
+    const page = await this.newPage();
     try {
       Logger.info('访问站点', link);
       await page.setViewport({ width: 1600, height: 900 });
