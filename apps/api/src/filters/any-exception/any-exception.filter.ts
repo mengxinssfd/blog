16a0fc7ae880f0362d98ea719d9d90c312504d08
dyh_a <xinzon32177@163.com>
2023-05-25 05:40:27 +0800

    feat(apps/api): 美化过滤器的error log

--- apps/api/src/filters/any-exception/any-exception.filter.ts
+++ apps/api/src/filters/any-exception/any-exception.filter.ts
@@ -1,7 +1,7 @@
 import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { Logger } from '@/utils/log4js';
-import { formatResLog } from '@/utils/format-log';
+import { formatReqLog, formatResLog } from '@/utils/format-log';
 import { shadowObj } from '@tool-pack/basic';
 
 @Catch()
@@ -12,10 +12,19 @@ export class AnyExceptionFilter implements ExceptionFilter {
     const statusCode =
       exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
 
-    const body = { code: statusCode, msg: `Service Error: ${exception}` };
+    const body = { code: statusCode, msg: exception.message };
 
     const req = shadowObj(ctx.getRequest<Request>(), { statusCode });
-    Logger.error(formatResLog(req, response, { ...body, data: exception.stack }), exception);
+    Logger.error(
+      JSON.stringify(req.headers, null, 4),
+      '\n',
+      formatReqLog(req).trimEnd(),
+      '\n',
+      formatResLog(req, response, body),
+      '\n',
+      exception,
+      '\n',
+    );
 
     response.status(statusCode).json(body);
   }
