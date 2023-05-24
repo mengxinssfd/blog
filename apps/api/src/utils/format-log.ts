0c86d3b1a3ab371a120c929b08117cb7b859c872
dyh_a <xinzon32177@163.com>
2023-05-25 03:53:58 +0800

    feat(apps/api): logger记录ip所在地

--- apps/api/src/utils/format-log.ts
+++ apps/api/src/utils/format-log.ts
@@ -2,13 +2,17 @@ import type { Request, Response } from 'express';
 import type { IncomingMessage } from 'http';
 import { ServerResponse } from 'http';
 import { getIp } from '@/utils/utils';
+import { Ip2RegionService } from '@/modules/ip2region/ip2region.service';
 
+const ip2Region = new Ip2RegionService();
 export function formatReqLog(req: Request): string {
+  const ip = getIp(req);
   // 组装日志信息
   return ` >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     Request original url: ${req.originalUrl}
     Method: ${req.method}
-    IP: ${getIp(req)}
+    IP: ${ip}
+    region: ${ip2Region.searchRawRegion(ip)}
     uuid: ${req.headers['uuid']}
     refer: ${req.headers.referer}
     Params: ${JSON.stringify(req.params)}
@@ -28,10 +32,12 @@ export function formatResLog(
   res: Response | ServerResponse,
   data: any,
 ): string {
+  const ip = getIp(req);
   return ` <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     Request original url: ${req.originalUrl}
     Method: ${req.method}
-    IP: ${getIp(req)}
+    IP: ${ip}
+    region: ${ip2Region.searchRawRegion(ip)}
     Status code: ${res.statusCode}
     User: ${JSON.stringify((req as any).user)}
     Response: ${JSON.stringify(data)}
