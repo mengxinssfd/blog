2071c8f352dc1cd462be3f41f69813ab7cb17b63
dyh_a <xinzon32177@163.com>
2023-05-24 20:19:46 +0800

    chore(pkgs/shared): 移除log

--- packages/shared/src/utils/getArticleLink.ts
+++ packages/shared/src/utils/getArticleLink.ts
@@ -1,7 +1,6 @@
 import type { CommentEntity } from '@blog/entities';
 
 export function getArticleLink(comment: CommentEntity): string {
-  console.log(comment);
   if (comment.scope) return '/' + comment.scope;
   return comment.article.as ? `/${comment.article.as}` : '/article/detail/' + comment.articleId;
 }
