ad36a96a6e23ad4e4fcb40036d6fc92cf0dda4df
dyh_a <xinzon32177@163.com>
2023-05-25 22:25:00 +0800

    fix(apps/front): 修复howLongAgo依据真实日数导致不符合口语的日期显示
    例如今天是12点、过去日期是前天18点，但因为不满2天会显示为昨天

--- apps/front/feature/utils/utils.ts
+++ apps/front/feature/utils/utils.ts
@@ -1,9 +1,22 @@
-import { howLongAgo as HowLongAgo, formatDate, inRange } from '@tool-pack/basic';
+import {
+  howLongAgo as HowLongAgo,
+  formatDate,
+  inRange,
+  getTimePeriodConst,
+} from '@tool-pack/basic';
 import { TupleM2N } from '@tool-pack/types';
 
 export const howLongAgo = (date: string | Date, format?: string) => {
   const _date = typeof date === 'string' ? new Date(date) : date;
   const now = new Date();
+
+  const dayDiff =
+    new Date(now.getFullYear(), now.getMonth(), now.getDate()).getTime() -
+    new Date(_date.getFullYear(), _date.getMonth(), _date.getDate()).getTime();
+
+  const match = { 1: '昨天', 2: '前天' }[~~(dayDiff / getTimePeriodConst().day)];
+  if (match) return match + formatDate(_date, ' hh:mm');
+
   return HowLongAgo(_date, {
     defaultFormat: format,
     def: '--',
@@ -17,8 +30,6 @@ export const howLongAgo = (date: string | Date, format?: string) => {
       if (inRange(diff, [0, 1000 * 10])) return '刚刚';
       // if (res.endsWith('天前')) return { 1: '昨天', 2: '前天' }[res.replace('天前', '')] || res;
       if (res.endsWith('天前')) {
-        const match = { '1天前': '昨天', '2天前': '前天' }[res];
-        if (match) return match + formatDate(_date, ' hh:mm');
         return formatDate(
           _date,
           now.getFullYear() === _date.getFullYear() ? 'MM-dd hh:mm:ss' : undefined,
