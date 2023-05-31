65f558807144bde0e644941a4e9dad4dd2e54117
dyh_a <xinzon32177@163.com>
2023-05-31 16:48:59 +0800

    fix(pkgs/dtos): `CreateCategoryDto`未继承CategoryEntity，未被基类约束

--- packages/dtos/src/category/create-category.dto.ts
+++ packages/dtos/src/category/create-category.dto.ts
@@ -1,16 +1,17 @@
 import { IsNotEmpty, IsString, Length } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
-import { BaseDto } from '../base.dto';
+import { PartialType } from '@nestjs/mapped-types';
+import { CategoryEntity } from '@blog/entities';
 
-export class CreateCategoryDto extends BaseDto<CreateCategoryDto> {
+export class CreateCategoryDto extends PartialType(CategoryEntity) {
   @ApiProperty({ description: '分类名', example: 'Typescript' })
   @IsNotEmpty({ message: '名称不能为空' })
   @Length(2, 24, { message: '名称长度必须在2-24之间' })
   @IsString({ message: '名称必须是字符串' })
-  name!: string;
+  override name!: string;
 
   @ApiProperty({ description: '分类描述', example: 'Javascript超集' })
   @IsString({ message: '分类描述必须是字符串' })
   @Length(2, 255, { message: '分类描述长度必须在2-255之间' })
-  description!: string;
+  override description!: string;
 }
