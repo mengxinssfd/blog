84429f48d76d06d8ad8723afd5c422563242e203
dyh_a <xinzon32177@163.com>
2024-02-05 23:52:10 +0800

    chore(front): 调整灯笼位置

--- apps/front/layouts/default.vue
+++ apps/front/layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
-  <Lantern style="left: 10px; top: 80px" content="春" />
+  <Lantern style="left: 10px" line-height="120px" content="春" />
   <Lantern style="left: 150px" left content="节" />
   <Lantern style="right: 150px" content="快" />
-  <Lantern style="right: 10px; top: 80px" content="乐" />
+  <Lantern style="right: 10px" line-height="120px" content="乐" />
   <Firework />
   <Header />
   <div class="page-wrapper _ flex-col">
